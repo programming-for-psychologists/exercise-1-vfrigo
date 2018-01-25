fbaeed5b6608bb0c75d6cb83f822d1fc74c46da6
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-01-25 10:54:45 -0600

    Remove unneeded imports

--- square.py
+++ square.py
@@ -1,6 +1,5 @@
-import time
 import sys
-from psychopy import visual,event,core
+from psychopy import visual, core
  
 win = visual.Window([400,400],color="black", units='pix')
 square = visual.Rect(win,lineColor="black",fillColor="blue",size=[100,100])
